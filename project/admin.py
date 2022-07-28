3480824ca8bd0c7b237ad9ce2d277b65f558f2f8
nuelxelence <nuelxelence@gmail.com>
2022-07-28 22:49:19 +0100

    updating

--- project/admin.py
+++ project/admin.py
@@ -4,14 +4,18 @@ from django.contrib import admin
 # Register your models here.
 
 from .models import *
-
-
-
-
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
+
+
+
+
 class ContactUsAdmin(admin.ModelAdmin):
     list_display = ['name','email','phone','selected_properties', 'message']
+
+
+
+admin.register.site(ProjectDetails)
